b5438381dcebfffd92be5f1dfcd7003ad286ac3d
joelneleber <joeyneleber@gmail.com>
2021-11-19 01:27:34 -0500

    Updated with logging (last state bug fix tomorrow)

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-import copy
+# File Management
+import glob
+import os
 
-import numpy as np
-import string
+# Variable assistance
 import random
+import string
+
+# Board manipulation
+import numpy as np
+import copy
 
 # Constants for pieces
 BLACK = -1
 WHITE = 1
 
 debug = False
+logging = False
 
 # Array values to string for printing
 piece_string = {
@@ -20,20 +27,20 @@ piece_string = {
 }
 
 
-def view_coords_board_coords(input_coords: str) -> (int, int):
+def view_coordinates_board_coordinates(input_coordinates: str) -> (int, int):
     """
-    Converts user entered coordinates (comma seperated) into two ints
+    Converts user entered coordinates (comma separated) into two ints
 
-    :param input_coords: user input in the following form: 0a,1b or a0,1b
+    :param input_coordinates: user input in the following form: 0a,1b or a0,1b
     :return: a array coordinate equivalent
     """
-    input_coords = input_coords.upper().strip().replace(" ", "")
-    if input_coords[0] in string.ascii_uppercase:
-        x = string.ascii_uppercase.find(input_coords[0])
-        y = int(input_coords[1])
+    input_coordinates = input_coordinates.upper().strip().replace(" ", "")
+    if input_coordinates[0] in string.ascii_uppercase:
+        x = string.ascii_uppercase.find(input_coordinates[0])
+        y = int(input_coordinates[1])
     else:
-        x = string.ascii_uppercase.find(input_coords[1])
-        y = int(input_coords[0])
+        x = string.ascii_uppercase.find(input_coordinates[1])
+        y = int(input_coordinates[0])
     return x, y
 
 
@@ -65,16 +72,21 @@ def add_move(dic, key, value):
 
 class Board:
     board_array = np.zeros((8, 8), dtype=int)
+    last_board_array = np.zeros((8,8), dtype=int)
     white_move: bool = True
     game_over: bool = False
     white_wins: bool = False
     draw: bool = False
     stop_probability: float = 0
-    white_policy = 1
-    black_policy = 0
+    white_policy: int = 1
+    black_policy: int = 0
+    to_file = None
+    last_move = ((0, 0), (0, 0))
 
     # Setup the board in the default configuration
-    def __init__(self):
+    def __init__(self, outfile = None):
+        if logging:
+            self.to_file = outfile
         for i in range(8):
             for j in range(8):
                 if i % 2 == 0:
@@ -122,7 +134,16 @@ class Board:
             print(str(i) + "  ", end="")
         print()
 
-    def get_single_moves(self, y: int, x: int) -> dict[(int, int):list[(int, int)]]:
+    def log(self):
+        to_file.write(np.array2string(current_board.last_board_array, threshold=100, max_line_width=np.inf,
+                                      separator=',').replace('\n', '') + "\t")  # Write the last_board
+        last_reward = current_board.reward()
+        to_file.write(str(current_board.last_move) + "\t")  # Write the last move
+        to_file.write(np.array2string(current_board.board_array, threshold=100, max_line_width=np.inf,
+                                      separator=',').replace('\n', '') + "\t")  # Write the new board
+        to_file.write(str(last_reward - current_board.reward()) + "\n")  # Write the reward change from this move
+
+    def get_single_moves(self, y: int, x: int):
         """
         Calculates all of the moves(captures) that a single piece can make
         This function is used when the player captures a piece and needs to see if they can move that piece again
@@ -136,9 +157,8 @@ class Board:
 
         current_color = WHITE if self.white_move else BLACK
         y_change = -current_color
-
         # Non-King
-        if abs(self.board_array[x][y]) == 1:
+        if abs(self.board_array[y][x]) == 1:
             if verify_location(y + y_change * 2, x + 2) and \
                     (self.board_array[y + y_change][x + 1] == current_color * -1 or
                      self.board_array[y + y_change][x + 1] == current_color * -2) and \
@@ -172,10 +192,11 @@ class Board:
                     self.board_array[y - 2][x - 2] == 0:
                 to_return[(y, x)].append((y - 2, x - 2))
         if debug:
+            self.print_board()
             print("potential next moves:", to_return)
         return to_return
 
-    def get_possible_moves(self) -> dict[(int, int):list[(int, int)]]:
+    def get_possible_moves(self):
         """
         This will iterate through all of their current pieces and list actions that each piece can take.
         :return: A dictionary of the locations to destinations (start_x,start_y)->[(new_location_x, new_location_y),...]
@@ -185,15 +206,15 @@ class Board:
         x, y, ky, kx = list(), list(), list(), list()
 
         # x and y values of the current board pieces
-        nonKingMoves = np.where(self.board_array == current_color)
-        if len(nonKingMoves) == 2:
-            y = nonKingMoves[0]
-            x = nonKingMoves[1]
+        non_king_moves = np.where(self.board_array == current_color)
+        if len(non_king_moves) == 2:
+            y = non_king_moves[0]
+            x = non_king_moves[1]
 
-        kingMoves = np.where(self.board_array == current_color * 2)
-        if len(nonKingMoves) == 2:
-            ky = kingMoves[0]
-            kx = kingMoves[1]
+        king_moves = np.where(self.board_array == current_color * 2)
+        if len(non_king_moves) == 2:
+            ky = king_moves[0]
+            kx = king_moves[1]
         possible_moves = dict()
         # Moving Non-king
         for i in range(len(y)):
@@ -253,7 +274,7 @@ class Board:
 
         # No pieces left, game over
         if len(possible_moves) == 0:
-            self.endGame()
+            self.endgame()
 
         return possible_moves
 
@@ -269,6 +290,9 @@ class Board:
         :param auto: is this game being played automatically or by a user
         :return:
         """
+        # Greatly slows down program but makes single statement logging possible
+        if logging:
+            self.last_board_array = copy.deepcopy(self.board_array)
 
         if is_secondary:
             possible_moves = self.get_single_moves(x, y)
@@ -283,6 +307,7 @@ class Board:
             self.game_over = True
 
         if (x, y) in possible_moves and (next_x, next_y) in possible_moves[(x, y)]:
+            self.last_move = ((x, y), (next_x, next_y))
             current_value = self.board_array[x][y]
             self.board_array[x][y] = 0
 
@@ -299,14 +324,17 @@ class Board:
                 if debug:
                     print("Capture")
                 self.board_array[int((x + next_x) / 2)][int((y + next_y) / 2)] = 0
-
                 next_moves = self.get_single_moves(next_x, next_y)
+                if logging:
+                    self.log()
                 self.play(auto, next_moves, True)
+            elif logging:
+                    self.log()
 
             return True
         return False
 
-    def play(self, auto: bool, actions: dict[(int, int):list[(int, int)]], secondary: bool):
+    def play(self, auto: bool, actions, secondary: bool):
         """
         This is the brain of the game, it will follow a policy or allow the user to input the moves
 
@@ -318,7 +346,7 @@ class Board:
         if auto:
             # Tie if there are no moves left
             if len(actions.keys()) == 0:
-                self.endGame()
+                self.endgame()
                 return False
 
             if self.white_move:
@@ -332,6 +360,8 @@ class Board:
                 elif self.black_policy == 1:
                     self.one_step_lookahead_policy(actions, secondary)
         else:
+            self.print_board()
+            print(f"Moves: {actions}")
             if secondary:
                 if len(actions) != 1 or len(actions[random.choice(list(actions.keys()))]) != 0:
                     inp = input("Capture! Enter next move (a0,b1) or 'P' to pass: ")
@@ -340,28 +370,30 @@ class Board:
                             print("Passing")
                     else:
                         current_piece, attempted_move = inp.split(",")
-                        current_piece = view_coords_board_coords(current_piece)
-                        attempted_move = view_coords_board_coords(attempted_move)
+                        current_piece = view_coordinates_board_coordinates(current_piece)
+                        attempted_move = view_coordinates_board_coordinates(attempted_move)
                         self.move(current_piece[0], current_piece[1], attempted_move[0], attempted_move[1], True, False)
             else:
-                board_coords_old, board_coords_new = input("Enter move (a0,b1): ").split(",")
-                board_coords_old = view_coords_board_coords(board_coords_old)
-                board_coords_new = view_coords_board_coords(board_coords_new)
+                board_coordinates_old, board_coordinates_new = input("Enter move (a0,b1): ").split(",")
+                board_coordinates_old = view_coordinates_board_coordinates(board_coordinates_old)
+                board_coordinates_new = view_coordinates_board_coordinates(board_coordinates_new)
 
                 # If input is not valid, try again
-                if board_coords_old not in actions.keys() or board_coords_new not in actions[board_coords_old]:
+                if board_coordinates_old not in actions.keys() or \
+                        board_coordinates_new not in actions[board_coordinates_old]:
                     print("Invalid move, try again.")
                     self.play(False, actions, False)
 
-                self.move(board_coords_old[0], board_coords_old[1], board_coords_new[0], board_coords_new[1], secondary,
+                self.move(board_coordinates_old[0], board_coordinates_old[1],
+                          board_coordinates_new[0], board_coordinates_new[1], secondary,
                           False)
 
-        # If this is not a secondary move, flip whose turn it is (this happens after all seconary moves are made)
+        # If this is not a secondary move, flip whose turn it is (this happens after all secondary moves are made)
         if not secondary:
             self.white_move = not self.white_move  # Flip whose turn it is
         return True
 
-    def endGame(self):
+    def endgame(self):
         """
         Prints out who won the game
 
@@ -387,7 +419,7 @@ class Board:
         """
         return sum(sum(self.board_array))
 
-    def random_policy(self, actions: dict[(int, int):list[(int, int)]], secondary):
+    def random_policy(self, actions, secondary):
         """
         Takes a random move from a random piece
 
@@ -406,7 +438,7 @@ class Board:
             else:
                 self.move(random_piece[0], random_piece[1], piece_move[0], piece_move[1], secondary, True)
 
-    def one_step_lookahead_policy(self, actions: dict[(int, int):list[(int, int)]], secondary):
+    def one_step_lookahead_policy(self, actions, secondary):
         """
         Calculates the reward from each piece and each action then takes the greatest rewards move.
 
@@ -416,14 +448,14 @@ class Board:
         :param secondary: is this a subsequent move
         :return: void
         """
-        rewards: dict[int:list[((int, int), (int, int))]] = dict()
+        rewards = dict()
         c_board = copy.deepcopy(self.board_array)
         for piece in actions.keys():
             for move in actions[piece]:
                 temp = Board()
                 temp.board_array = copy.deepcopy(self.board_array)
                 temp.white_move = self.white_move
-                temp.move(piece[0], piece[1], move[0], move[1], False, True)
+                temp.move(piece[0], piece[1], move[0], move[1], secondary, True)
                 add_move(rewards, temp.reward(), (piece, move))
         self.board_array = c_board
 
@@ -441,37 +473,83 @@ class Board:
                       secondary, True)
 
 
+def get_next_csv_number() -> int:
+    # ---- Finding the last file name ----
+    current_file_path = os.path.dirname(__file__)
+
+    # Getting the names of all isolated shots
+    last_file_name = glob.glob(current_file_path + "/game_records/*.tsv")
+    next_file_name = 0
+    # Finding the number of the last isolated shot
+    isolated_shots = []
+    if last_file_name:
+        for index, item in enumerate(last_file_name):
+            file_number = int(item[item.rfind("/") + 1: -4])
+            if next_file_name <= file_number:
+                next_file_name = file_number + 1
+    else:
+        next_file_name = 0
+
+    return next_file_name
+
+
 if __name__ == "__main__":
     draws = 0
     black_wins = 0
     white_wins = 0
-    for _ in range(1000):
+    y = np.array([[-1, 0, 2, 0, 0, 0,-1, 0], [ 0, 0, 0,-1, 0,-1, 0,-1], [ 0, 0, 0, 0,-1, 0,-1, 0], [ 0,-1, 0,-1, 0, 0, 0,-1], [ 0, 0, 0, 0, 1, 0, 1, 0], [ 0, 1, 0, 1, 0, 1, 0, 0], [ 1, 0, 0, 0, 1, 0, 1, 0], [ 0, 1, 0, 1, 0, 0, 0, 1]])
+    x = np.array([[-1, 0, 2, 0, 2, 0,-1, 0], [ 0, 0, 0, 0, 0,-1, 0,-1], [ 0, 0, 0, 0,-1, 0,-1, 0], [ 0,-1, 0, 0, 0, 0, 0,-1], [ 0, 0, 0, 0, 0, 0, 1, 0], [ 0, 1, 0, 1, 0, 1, 0, 0], [ 1, 0, 0, 0, 1, 0, 1, 0], [ 0, 1, 0, 1, 0, 0, 0, 1]])
+    for _ in range(10):
         current_board = Board()
+        current_board.board_array = x
+        current_board.print_board()
+        current_board.board_array = y
+        current_board.print_board()
+        break
         current_board.black_policy = 1
         current_board.white_policy = 1
         still_playing: bool = True
-        while still_playing:
-            #  White
-            # current_board.print_board()
-            moves = current_board.get_possible_moves()
-            # print("Moves:", moves)
-            still_playing = current_board.play(True, moves, False)
+        with open("./game_records/" + str(get_next_csv_number()) + ".tsv", "w") as to_file:
+            while still_playing:
+                #  White
+                to_file.write(np.array2string(current_board.board_array, threshold=100, max_line_width=np.inf,
+                                              separator=',').replace('\n', '') + "\t")  # Write the current board
+                old_reward = current_board.reward()
 
-            if not still_playing:
-                break
-            #  Black
-            # current_board.print_board()
-            moves = current_board.get_possible_moves()
-            # print("Moves:", moves)
-            still_playing = current_board.play(True, current_board.get_possible_moves(), False)
+                #  Playing
+                moves = current_board.get_possible_moves()
+                still_playing = current_board.play(True, moves, False)
+                #  Recording
+                to_file.write(str(current_board.last_move) + "\t")  # Write the last move
+                to_file.write(np.array2string(current_board.board_array, threshold=100, max_line_width=np.inf,
+                                              separator=',').replace('\n', '') + "\t")  # Write the new board
+                to_file.write(str(old_reward - current_board.reward()) + "\n")  # Write the reward change from this move
 
-        if current_board.draw:
-            draws += 1
-        elif current_board.white_wins:
-            white_wins += 1
-        else:
-            black_wins += 1
-        # current_board.print_board()
+                if not still_playing:
+                    break
+
+                #  Black
+                old_reward = current_board.reward()
+                to_file.write(np.array2string(current_board.board_array, threshold=100, max_line_width=np.inf,
+                                              separator=',').replace('\n', '') + "\t")  # Write the current board
+
+                #  Playing
+                moves = current_board.get_possible_moves()
+                still_playing = current_board.play(True, current_board.get_possible_moves(), False)
+
+                #  Recording
+                to_file.write(str(current_board.last_move)+ "\t")  # Write the last move
+                to_file.write(np.array2string(current_board.board_array, threshold=100, max_line_width=np.inf,
+                                              separator=',').replace('\n', '') + "\t")  # Write the new board
+                to_file.write(str(old_reward - current_board.reward()) + "\n")  # Write the reward change from this move
+
+            if current_board.draw:
+                draws += 1
+            elif current_board.white_wins:
+                white_wins += 1
+            else:
+                black_wins += 1
+            current_board.print_board()
 
     print("Draws:", draws)
     print("White wins:", white_wins)
